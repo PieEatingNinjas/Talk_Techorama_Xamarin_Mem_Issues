f642f21a6d480c7973021daeba945e0967a7506a
Bruno VT <bruno.vanthournout@ordina.be>
2018-04-13 13:10:14 +0200

    initial commit for the Android demo

---
+++ DroidDemo/Droid/Views/Activities/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Ordina.Techorama_2018.Memory.Droid;
+
+namespace Ordina.Techorama.Memory.Droid.Views.Activities
+{
+    [Activity(MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.ActivityMain);
+        }
+    }
+}
+
