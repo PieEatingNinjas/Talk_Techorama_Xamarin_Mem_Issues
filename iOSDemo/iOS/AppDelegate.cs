3e61eac924dc462916a0399d4ec70fefd75e7c13
Bruno VT <bruno.vanthournout@ordina.be>
2018-04-19 10:46:31 +0200

    iOS demo initial commit

---
+++ iOSDemo/iOS/AppDelegate.cs
@@ -0,0 +1,26 @@
+﻿using Foundation;
+using UIKit;
+using Ordina.Techorama_2018.Memory.iOS.ViewControllers;
+
+namespace Ordina.Techorama_2018.Memory.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        UIWindow _window;
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            _window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            var navigationController = new UINavigationController(new MainViewController());
+
+            _window.RootViewController = navigationController;
+
+            _window.MakeKeyAndVisible();
+
+            return true;
+        }
+    }
+}
+
---
+++ iOSDemo/iOS/ViewControllers/MainViewController.cs
@@ -0,0 +1,31 @@
+﻿using CoreGraphics;
+using UIKit;
+
+namespace Ordina.Techorama_2018.Memory.iOS.ViewControllers
+{
+    public class MainViewController : UIViewController
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            NavigationItem.Title = "Main";
+
+            var mainView = new UIView
+            {
+                Frame = UIScreen.MainScreen.Bounds,
+                BackgroundColor = UIColor.White
+            };
+
+            var firstButton = new UIButton
+            {
+                Frame = new CGRect(mainView.Frame.Width / 2 - 100, 120, 200, 44)
+            };
+            firstButton.SetTitle("Go to gallery", UIControlState.Normal);
+            firstButton.SetTitleColor(UIColor.Blue, UIControlState.Normal);
+            mainView.AddSubview(firstButton);
+
+            View = mainView;
+        }
+    }
+}
