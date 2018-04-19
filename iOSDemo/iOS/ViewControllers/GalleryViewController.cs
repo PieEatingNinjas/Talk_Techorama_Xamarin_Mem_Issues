61ff412e409b11336a1d7a6cdb1bafb028cc4240
Bruno VT <bruno.vanthournout@ordina.be>
2018-04-19 11:28:00 +0200

    ios imagegallery page

---
+++ iOSDemo/iOS/ViewControllers/GalleryViewController.cs
@@ -0,0 +1,53 @@
+﻿using UIKit;
+using System.Collections.Generic;
+
+namespace Ordina.Techorama_2018.Memory.iOS.ViewControllers
+{
+    public class GalleryViewController : UIViewController
+    {
+        List<string> _images;
+        int _currentImageIndex;
+
+        ImageViewController _imageViewController;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            NavigationItem.Title = "Gallery";
+
+            var mainView = new UIView
+            {
+                Frame = UIScreen.MainScreen.Bounds,
+                BackgroundColor = UIColor.White
+            };
+            View = mainView;
+
+            _imageViewController = new ImageViewController(this);
+            AddChildViewController(_imageViewController);
+            mainView.AddSubview(_imageViewController.View);
+            _imageViewController.DidMoveToParentViewController(this);
+
+            _currentImageIndex = 0;
+            _images = new List<string>
+            {
+                "Xamarin",
+                "Ordina",
+                "Microsoft",
+                "Os"
+            };
+
+            ShowNextImage();
+        }
+
+        public void ShowNextImage()
+        {
+            _imageViewController.LoadImage(_images[_currentImageIndex]);
+
+            _currentImageIndex++;
+
+            if (_currentImageIndex >= _images.Count)
+                _currentImageIndex = 0;
+        }
+    }
+}
--- iOSDemo/iOS/ViewControllers/MainViewController.cs
+++ iOSDemo/iOS/ViewControllers/MainViewController.cs
@@ -1,10 +1,13 @@
-﻿using CoreGraphics;
+﻿using System;
+using CoreGraphics;
 using UIKit;
 
 namespace Ordina.Techorama_2018.Memory.iOS.ViewControllers
 {
     public class MainViewController : UIViewController
     {
+        UIButton _galleryButton;
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
@@ -16,16 +19,36 @@ namespace Ordina.Techorama_2018.Memory.iOS.ViewControllers
                 Frame = UIScreen.MainScreen.Bounds,
                 BackgroundColor = UIColor.White
             };
+            View = mainView;
 
-            var firstButton = new UIButton
+            _galleryButton = new UIButton
             {
                 Frame = new CGRect(mainView.Frame.Width / 2 - 100, 120, 200, 44)
             };
-            firstButton.SetTitle("Go to gallery", UIControlState.Normal);
-            firstButton.SetTitleColor(UIColor.Blue, UIControlState.Normal);
-            mainView.AddSubview(firstButton);
+            _galleryButton.SetTitle("Go to gallery", UIControlState.Normal);
+            _galleryButton.SetTitleColor(UIColor.Blue, UIControlState.Normal);
 
-            View = mainView;
+            _galleryButton.TouchUpInside += GalleryButton_TouchUpInside;
+
+            mainView.AddSubview(_galleryButton);
+        }
+
+        void GalleryButton_TouchUpInside(object sender, EventArgs e)
+        {
+            NavigationController.PushViewController(new GalleryViewController(), true);
+        }
+
+        bool _disposed;
+        protected override void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+
+                _galleryButton.TouchUpInside -= GalleryButton_TouchUpInside;
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
